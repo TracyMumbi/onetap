fc72f1f2171fb86f9da3ff4d16c431bbeb7a9622
Tracy Mumbi <tracychiuri@gmail.com>
2023-08-10 19:53:05 +0300

    onetap sign in

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import {useGoogleOneTapLogin} from "react-google-one-tap-login"
 
 function App() {
+  const [name, setName] = useState([])
+  const [email, setEmail] = useState([])
+  const [image, setImage] = useState([])
+  const [email_verified, setEmail_Verified] = useState([])
+  useGoogleOneTapLogin({
+    onSuccess: (response) => {
+      setName(response.name)
+      setEmail(response.email)
+      setEmail_Verified(response.email_verified)
+      setImage(response.picture)
+    },
+    onError: (error) => console.log(error),
+    googleAccountConfigs: {
+      client_id: "714677259140-dhujfnhm4c8sqosahh9ivjgc7lt5j6dn.apps.googleusercontent.com",
+    },
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+     {name}<br/>
+     {email}<br/>
+     {email_verified}<br/>
+     {image}<br/>
     </div>
   );
 }
